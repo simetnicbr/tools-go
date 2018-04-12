3835e06de1f92fd307f30d23881b8debc6b1e6f2
Fabio Nokai <fnokai@nic.br>
2018-04-12 11:07:03 -0300

    Add logger to tools-go

--- logger/logger.go
+++ logger/logger.go
@@ -23,9 +23,9 @@ type Logger interface {
 	Info(m ...interface{})
 	Debug(m ...interface{})
 
-	Warningm(map[string]interface{})
-	Infom(map[string]interface{})
-	Debugm(map[string]interface{})
+	Warningm(map[string]interface{}, ...interface{})
+	Infom(map[string]interface{}, ...interface{})
+	Debugm(map[string]interface{}, ...interface{})
 
 	Warningf(f string, m ...interface{})
 	Infof(f string, m ...interface{})
@@ -63,70 +63,58 @@ func (l logWrapper) SetDepth(d int) {
 	l.Depth = d
 }
 
-func (l logWrapper) Warning(m ...interface{}) {
-	msg := l.prefix(fmt.Sprint(m...))
-
-	l.Logger.Warning(msg)
+func (l logWrapper) Warning(msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Warning(msg)
 }
 
-func (l logWrapper) Info(m ...interface{}) {
-	msg := l.prefix(fmt.Sprint(m...))
-
-	l.Logger.Info(msg)
+func (l logWrapper) Info(msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Info(msg)
 }
 
-func (l logWrapper) Debug(m ...interface{}) {
-	msg := l.prefix(fmt.Sprint(m...))
-
-	l.Logger.Debug(msg)
+func (l logWrapper) Debug(msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Debug(msg)
 }
 
-func (l logWrapper) Warningm(m map[string]interface{}) {
-	m["metadata"] = l.metadata()
-	l.Logger.WithFields(m).Warning()
+func (l logWrapper) Warningm(m map[string]interface{}, msg ...interface{}) {
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Warning(msg...)
 }
 
-func (l logWrapper) Infom(m map[string]interface{}) {
-	m["metadata"] = l.metadata()
-	l.Logger.WithFields(m).Info()
+func (l logWrapper) Infom(m map[string]interface{}, msg ...interface{}) {
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Info(msg...)
 }
 
-func (l logWrapper) Debugm(m map[string]interface{}) {
-	m["metadata"] = l.metadata()
-	l.Logger.WithFields(m).Debug()
+func (l logWrapper) Debugm(m map[string]interface{}, msg ...interface{}) {
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Debug(msg...)
 }
 
-func (l logWrapper) Warningf(f string, m ...interface{}) {
-	msg := l.prefix(fmt.Sprintf(f, m...))
-
-	l.Logger.Warningf(msg)
+func (l logWrapper) Warningf(f string, msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Warning(fmt.Sprintf(f, msg...))
 }
 
-func (l logWrapper) Infof(f string, m ...interface{}) {
-	msg := l.prefix(fmt.Sprintf(f, m...))
-
-	l.Logger.Info(msg)
+func (l logWrapper) Infof(f string, msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Info(fmt.Sprintf(f, msg...))
 }
 
-func (l logWrapper) Debugf(f string, m ...interface{}) {
-	msg := l.prefix(fmt.Sprintf(f, m...))
-
-	l.Logger.Debug(msg)
+func (l logWrapper) Debugf(f string, msg ...interface{}) {
+	var m map[string]interface{}
+	m["file"] = l.file()
+	l.Logger.WithFields(m).Debugf(fmt.Sprintf(f, msg...))
 }
 
-func (l logWrapper) prefix(msg string) string {
-	_, filepath, line, _ := runtime.Caller(l.Depth)
-
-	tokens := strings.Split(filepath, "/")
-	file := tokens[len(tokens)-1]
-	if len(tokens) >= 2 {
-		file = tokens[len(tokens)-2] + "/" + tokens[len(tokens)-1]
-	}
-
-	return fmt.Sprintf("{%v: %v} %v", file, line, msg)
-}
-
-func (l logWrapper) metadata() string {
+func (l logWrapper) file() string {
 	_, filepath, line, _ := runtime.Caller(l.Depth)
 
 	tokens := strings.Split(filepath, "/")
